26c2f6bfadea90f1c7fd6de1f2b96b71a551a1b6
Matthew David Elgert <mdelgert@yahoo.com>
2025-04-25 19:34:05 -0400

    Remove commented-out Weather API link from NavMenu and add ARM template for Azure deployment

--- WebAPI/Program.cs
+++ WebAPI/Program.cs
@@ -4,24 +4,32 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(); // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+//if (app.Environment.IsDevelopment())
+//{
+//    app.UseSwagger();
+//    app.UseSwaggerUI(ModernStyle.Dark);
+//}
+
+// Add middleware to redirect the root URL to Swagger UI
+app.Use(async (context, next) =>
 {
-    app.UseSwagger();
-    //app.UseSwaggerUI();
-    app.UseSwaggerUI(ModernStyle.Dark);
-}
+    if (context.Request.Path == "/")
+    {
+        context.Response.Redirect("/swagger");
+        return;
+    }
+    await next();
+});
 
+app.UseSwagger();
+app.UseSwaggerUI(ModernStyle.Dark);
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
 app.MapControllers();
-
 app.Run();
