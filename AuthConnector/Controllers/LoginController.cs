e15dd1789d67bfe6e73123c70ebdfca2de94fc0a
Matthew David Elgert <mdelgert@yahoo.com>
2025-05-14 18:30:26 -0400

    Add basic authentication to LoginController and update appsettings for credentials

--- AuthConnector/Controllers/LoginController.cs
+++ AuthConnector/Controllers/LoginController.cs
@@ -1,27 +1,38 @@
 ﻿using AuthConnector.Services;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
 
 namespace AuthConnector.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("[controller]")]
     [ApiController]
     public class LoginController : ControllerBase
     {
         private readonly DbService _context;
         private readonly ILogger<LoginController> _logger;
+        private readonly IConfiguration _configuration;
 
-        public LoginController(DbService context, ILogger<LoginController> logger)
+        public LoginController(DbService context, ILogger<LoginController> logger, IConfiguration configuration)
         {
             _context = context;
             _logger = logger;
+            _configuration = configuration;
         }
 
+        //https://github.com/Azure-Samples/active-directory-dotnet-external-identities-api-connector-azure-function-validate/blob/master/SignUpValidation.cs
         [HttpGet(Name = "GetLogin")]
         public async Task<IActionResult> Get()
         {
             try
             {
+                // Check HTTP basic authorization using the Request property
+                if (!Authorize())
+                {
+                    _logger.LogWarning("HTTP basic authentication validation failed.");
+                    return new UnauthorizedResult();
+                }
+
                 _logger.LogInformation("Login requested");
 
                 var log = new Models.LogModel
@@ -41,5 +52,46 @@ namespace AuthConnector.Controllers
 
             return Ok("Hello Login!");
         }
+
+        private bool Authorize()
+        {
+            // Get user name and password from the appsettings using IConfiguration
+            var username = _configuration["Auth:User"];
+            var password = _configuration["Auth:Pass"];
+
+            // Returns authorized if the username is empty or not exists.
+            if (string.IsNullOrEmpty(username))
+            {
+                _logger.LogInformation("HTTP basic authentication is not set.");
+                return true;
+            }
+
+            // Check if the HTTP Authorization header exist
+            if (!Request.Headers.ContainsKey("Authorization"))
+            {
+                _logger.LogWarning("Missing HTTP basic authentication header.");
+                return false;
+            }
+
+            // Read the authorization header
+            var auth = Request.Headers["Authorization"].ToString();
+
+            // Ensure the type of the authorization header id `Basic`
+            if (!auth.StartsWith("Basic "))
+            {
+                _logger.LogWarning("HTTP basic authentication header must start with 'Basic '.");
+                return false;
+            }
+
+            // Get the the HTTP basinc authorization credentials
+            var cred = System.Text.UTF8Encoding.UTF8.GetString(Convert.FromBase64String(auth.Substring(6))).Split(':');
+
+            // Log the credentials
+            _logger.LogInformation($"HTTP basic authentication credentials: {cred[0]}:{cred[1]}");
+
+            // Evaluate the credentials and return the result
+            return (cred[0] == username && cred[1] == password);
+        }
     }
+
 }
