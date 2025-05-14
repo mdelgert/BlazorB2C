6fa5f734709fa98a7703d95792c8e64936f2c70d
Matthew David Elgert <mdelgert@yahoo.com>
2025-05-14 15:07:46 -0400

    Add AuthConnector project with WeatherForecast API and logging; configure database context and Docker support

---
+++ AuthConnector/Program.cs
@@ -0,0 +1,45 @@
+using AspNetCore.Swagger.Themes;
+using AuthConnector.Services;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Configure the database context
+builder.Services.AddDbContext<DbService>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+// Add services to the container.
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+//if (app.Environment.IsDevelopment())
+//{
+//    app.UseSwagger();
+//    app.UseSwaggerUI();
+//}
+
+app.UseSwagger(c =>
+{
+    c.OpenApiVersion = Microsoft.OpenApi.OpenApiSpecVersion.OpenApi2_0;
+});
+app.UseSwaggerUI(ModernStyle.Dark);
+app.Use(async (context, next) =>
+{
+    if (context.Request.Path == "/")
+    {
+        context.Response.Redirect("/swagger");
+        return;
+    }
+    await next();
+});
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
---
+++ AuthConnector/Services/DbService.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using AuthConnector.Models;
+
+namespace AuthConnector.Services
+{
+    public class DbService : DbContext
+    {
+        public DbService(DbContextOptions<DbService> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<LogModel> Logs { get; set; }
+    }
+}
